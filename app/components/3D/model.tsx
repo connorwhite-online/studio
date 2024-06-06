d594de0fd6e602775c98b60276ccc65b8fcaa25c
Connor White <connorwhite@Connors-iMac.local>
2024-06-05 22:43:57 -0700

    scene simplified/new model

--- app/components/3D/model.tsx
+++ app/components/3D/model.tsx
@@ -1,5 +1,5 @@
 import * as THREE from 'three'
-import React, { use, useRef } from 'react'
+import React, { useRef } from 'react'
 import { useGLTF } from '@react-three/drei'
 import { GLTF } from 'three-stdlib'
 import gsap from 'gsap'
@@ -16,26 +16,31 @@ type GLTFResult = GLTF & {
     Top: THREE.Mesh
   }
   materials: {
-    ['Steel - Satin']: THREE.MeshPhysicalMaterial
-    Knob: THREE.MeshStandardMaterial
-    Lens: THREE.MeshPhysicalMaterial
+    ['Stainless Steel']: THREE.MeshStandardMaterial
+    ['Anodized Aluminum']: THREE.MeshPhysicalMaterial
+    ['ABS Plastic']: THREE.MeshStandardMaterial
+    ['Ceramic Glass Lens']: THREE.MeshPhysicalMaterial
   }
 }
 
 export function Model(props: JSX.IntrinsicElements['group']) {
   const { nodes, materials } = useGLTF('/device.gltf') as GLTFResult
+
   const group = useRef<THREE.Group>(null!)
   const tl = useRef<gsap.core.Timeline>()
 
   useGSAP(() => {
     tl.current = gsap.timeline({ yoyo: true, repeat: -1 })
     .fromTo(group.current.rotation, {
-      x: 2.542,
-      y: -0.833,
-      z: 1.502
+      // x: 2.542,
+      // y: -0.5,
+      // z: 1.502
+      x: 0,
+      y: Math.PI * 1.15,
+      z: -(Math.PI / 2)
     }, {
       // z: .1,
-      y: Math.PI * 3, 
+      y: Math.PI * 4.15, 
       duration: 6, 
       ease: 'power4.inOut',
       delay: 1,
@@ -43,62 +48,49 @@ export function Model(props: JSX.IntrinsicElements['group']) {
     
   }, { dependencies: [] })
 
-
   return (
-      <group 
-      ref={group} 
-      rotation={[2.542, -0.833, 1.102]} 
-      // rotation={[0, 0, 0]} 
-      scale={0.075} 
-      {...props} dispose={null}>
+    <group ref={group} {...props} dispose={null} rotation={[Math.PI / 2, -Math.PI / 2, 0]} scale={1.25}>
         <mesh
           castShadow
           receiveShadow
           geometry={nodes.Bolt_1.geometry}
-          material={materials['Steel - Satin']}
-          scale={10}
+          material={materials['Stainless Steel']}
         />
         <mesh
           castShadow
           receiveShadow
           geometry={nodes.Bolt_2.geometry}
-          material={materials['Steel - Satin']}
-          scale={10}
+          material={materials['Stainless Steel']}
         />
         <mesh
           castShadow
           receiveShadow
           geometry={nodes.Bottom.geometry}
-          material={materials['Steel - Satin']}
-          scale={10}
+          material={materials['Anodized Aluminum']}
         />
         <mesh
           castShadow
           receiveShadow
           geometry={nodes.Button.geometry}
-          material={materials['Steel - Satin']}
-          scale={10}
+          material={materials['Anodized Aluminum']}
         />
         <mesh
           castShadow
           receiveShadow
           geometry={nodes.Knob.geometry}
-          material={materials.Knob}
-          scale={10}
+          material={materials['ABS Plastic']}
         />
         <mesh
           castShadow
           receiveShadow
           geometry={nodes.Lidar_Lens.geometry}
-          material={materials.Lens}
-          scale={10}
+          material={materials['Ceramic Glass Lens']}
         />
         <mesh
           castShadow
           receiveShadow
           geometry={nodes.Top.geometry}
-          material={materials['Steel - Satin']}
-          scale={10}
+          material={materials['Anodized Aluminum']}
         />
     </group>
   )
--- app/components/3D/scene.tsx
+++ app/components/3D/scene.tsx
@@ -1,36 +1,17 @@
 import React from 'react';
-import * as Three from 'three';
 import { Canvas } from '@react-three/fiber';
-import { OrbitControls, GizmoHelper } from '@react-three/drei';
 import { Model } from './model';
-import { useSpring, a } from '@react-spring/three';
+import { Environment } from '@react-three/drei';
 
 const Scene: React.FC = () => {
-
-    const  groupRotation  = useSpring({
-        to: {
-            groupRotation: Math.PI / 2
-        },
-        config: {
-            mass: 5,
-            tension: 150,
-            friction: 50,
-        },
-    });
 
   return (
     <Canvas 
         style={{ width: "100dvw", height: "100dvh", zIndex: 98}}
         camera={{fov: 75, position: [0, 0, 5]}}
     >
-        {/* <OrbitControls /> */}
-        <GizmoHelper />
-        <ambientLight intensity={1} />
-        <directionalLight position={[-10, -10, -5]} intensity={1} />
-        <directionalLight position={[10, 10, 5]} intensity={1} />
-        {/* <a.group  rotation-y={groupRotation}> */}
-            <Model />
-        {/* </a.group> */}
+        <Model />
+        <Environment preset="studio" environmentIntensity={0.5}/>
     </Canvas>
   );
 };
