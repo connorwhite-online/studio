240c4de4867462f2b1578e6939b12bdb340fb007
Connor White <connorwhitepdx@gmail.com>
2024-07-04 11:37:53 -0700

    z-index shift

--- app/components/3D/intro.tsx
+++ app/components/3D/intro.tsx
@@ -2,7 +2,7 @@ import { Html } from "@react-three/drei";
 
 const Intro: React.FC = () => {
     return (
-        <div className="wrapper">
+        <div className="wrapper" style={{zIndex: 99}}>
             <div className='introCopy' style={{display: 'flex', flexDirection: 'column', gap: '10px'}}>
                 <div style={{display: 'flex', justifyContent: 'space-between', alignItems: 'center'}}><h3 style={{textWrap: 'nowrap'}}>CONNOR WHITE</h3><hr style={{width: '100%', margin: '0 5px'}}/><h3>STUDIO</h3></div>
                 <h2>Independent designer and engineer <br/> building interactive web experiences.</h2>
