0d7dbbbfeddd9eef00acad75a8183c5ef5aef03f
Connor White <connorwhitepdx@gmail.com>
2025-06-07 02:48:06 -0700

    ca

--- app/components/Bio/index.tsx
+++ app/components/Bio/index.tsx
@@ -8,7 +8,7 @@ export default function Bio() {
         Connor White
       </h1>
       <p>
-        I&#39;m a software designer and engineer based in Los Angeles.
+        I&#39;m a software designer and engineer based in Los Angeles, California.
         <br />
         Currently, I&#39;m a design engineer at <a href="https://www.tyb.xyz/" target="_blank" rel="noopener noreferrer" className={styles.inlineLink}>TYB</a> focusing on interaction design and obsessing over all the little details that make an app feel enjoyable to interact with.
       </p>
