56b4f6bf080e8e643e01bc89b99f7b0b83fc0526
Connor White <connorwhitepdx@gmail.com>
2025-09-03 16:21:01 -0700

    lil text restyle

--- app/components/CVWidget/index.tsx
+++ app/components/CVWidget/index.tsx
@@ -17,19 +17,19 @@ export default function CVWidget({ className = '' }: CVWidgetProps) {
         </div>
         <div className={styles.cvItem}>
             <div className={styles.cvItemOrg}>
-                <p>Try Your Best</p>
+                <h3>Try Your Best</h3>
                 <small>2024 - Present</small>
             </div>
-            <p style={{color: 'var(--gray-900)', fontFamily: 'var(--light)'}}>
+            <p>
                 I currently lead design engineering across all surface areas, spending 75% of my time in Figma, and the other 25% in code. Before that, I worked on the b2b client web-app, leading a massive redesign of the platform.     
             </p>
         </div>
         <div className={styles.cvItem}>
             <div className={styles.cvItemOrg}>
-                <p>Freelance/Contract</p>
+                <h3>Freelance/Contract</h3>
                 <small>2018 - Present</small>
             </div>
-            <p style={{color: 'var(--gray-900)', fontFamily: 'var(--light)'}}>
+            <p>
                 Client work across brand, web, and native, ranging from identity and full suite development to experimentation with machine learning and real-time 3D experiences on the web.
                 <br />
                 Past clients include <a href="https://www.instagram.com/byseanbrown" target="_blank" rel="noopener noreferrer" className={styles.inlineLink}>Sean Brown</a>, <a href="https://www.instagram.com/claudehome" target="_blank" rel="noopener noreferrer" className={styles.inlineLink}>Claude Home</a>, <a href="https://www.nike.com/" target="_blank" rel="noopener noreferrer" className={styles.inlineLink}>Nike</a>, and more.
